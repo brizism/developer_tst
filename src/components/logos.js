50f825ad8ca19b3ef23461817b54e39af73cd844
brizism <montielbriseida@gmail.com>
2018-04-27 18:46:23 -0400

    coin logos

---
+++ src/components/logos.js
@@ -0,0 +1,13 @@
+import BTC from '../logos/BTC.png';
+import ETH from '../logos/ETH.png';
+import XRP from '../logos/XRP.png';
+import BCH from '../logos/BCH.png';
+import EOS from '../logos/EOS.png';
+
+export default {
+  BTC,
+  ETH,
+  XRP,
+  BCH,
+  EOS
+}
