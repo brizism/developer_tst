6e85c7c22a5c93220211f7bd07ece94880b8bad2
brizism <montielbriseida@gmail.com>
2018-04-29 16:20:29 -0400

    import files

--- src/App.js
+++ src/App.js
@@ -1,4 +1,9 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Redirect, Switch, Route } from 'react-router-dom';
+import NavBar from './components/NavBar';
+import Home from './components/Home';
+import CoinPage from './components/CoinPage';
+import './styles/min_css/styles.css';
 
 class App extends Component {
   render() {
