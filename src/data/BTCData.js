2d6233a7d212920babaa8a2897ab249f90b038ab
brizism <montielbriseida@gmail.com>
2018-04-29 18:15:11 -0400

    bitcoin week data

---
+++ src/data/BTCData.js
@@ -0,0 +1,154 @@
+ const BTCData = [  
+  {  
+     "date":"3/30/2018",
+     "closing price":6890.52,
+     "opening price":7171.45
+  },
+  {  
+     "date":"3/31/2018",
+     "closing price":6973.53,
+     "opening price":6892.48
+  },
+  {  
+     "date":"4/1/2018",
+     "closing price":6844.23,
+     "opening price":7003.06
+  },
+  {  
+     "date":"4/2/2018",
+     "closing price":7083.8,
+     "opening price":6844.86
+  },
+  {  
+     "date":"4/3/2018",
+     "closing price":7456.11,
+     "opening price":7102.26
+  },
+  {  
+     "date":"4/4/2018",
+     "closing price":6853.84,
+     "opening price":7456.41
+  },
+  {  
+     "date":"4/5/2018",
+     "closing price":6811.47,
+     "opening price":6848.65
+  },
+  {  
+     "date":"4/6/2018",
+     "closing price":6636.32,
+     "opening price":6815.96
+  },
+  {  
+     "date":"4/7/2018",
+     "closing price":6911.09,
+     "opening price":6630.51
+  },
+  {  
+     "date":"4/8/2018",
+     "closing price":7023.52,
+     "opening price":6919.98
+  },
+  {  
+     "date":"4/9/2018",
+     "closing price":6770.73,
+     "opening price":7044.32
+  },
+  {  
+     "date":"4/10/2018",
+     "closing price":6834.76,
+     "opening price":6795.44
+  },
+  {  
+     "date":"4/11/2018",
+     "closing price":6968.32,
+     "opening price":6843.47
+  },
+  {  
+     "date":"4/12/2018",
+     "closing price":7889.25,
+     "opening price":6955.38
+  },
+  {  
+     "date":"4/13/2018",
+     "closing price":7895.96,
+     "opening price":7901.09
+  },
+  {  
+     "date":"4/14/2018",
+     "closing price":7986.24,
+     "opening price":7874.67
+  },
+  {  
+     "date":"4/15/2018",
+     "closing price":8329.11,
+     "opening price":7999.33
+  },
+  {  
+     "date":"4/16/2018",
+     "closing price":8058.67,
+     "opening price":8337.57
+  },
+  {  
+     "date":"4/17/2018",
+     "closing price":7902.09,
+     "opening price":8071.66
+  },
+  {  
+     "date":"4/18/2018",
+     "closing price":8163.42,
+     "opening price":7944.43
+  },
+  {  
+     "date":"4/19/2018",
+     "closing price":8294.31,
+     "opening price":8159.27
+  },
+  {  
+     "date":"4/20/2018",
+     "closing price":8845.83,
+     "opening price":8286.88
+  },
+  {  
+     "date":"4/21/2018",
+     "closing price":8895.58,
+     "opening price":8848.79
+  },
+  {  
+     "date":"4/22/2018",
+     "closing price":8802.46,
+     "opening price":8925.06
+  },
+  {  
+     "date":"4/23/2018",
+     "closing price":8930.88,
+     "opening price":8794.39
+  },
+  {  
+     "date":"4/24/2018",
+     "closing price":9697.5,
+     "opening price":8934.34
+  },
+  {  
+     "date":"4/25/2018",
+     "closing price":8845.74,
+     "opening price":9701.03
+  },
+  {  
+     "date":"4/26/2018",
+     "closing price":9281.51,
+     "opening price":8867.32
+  },
+  {  
+     "date":"4/27/2018",
+     "closing price":8987.05,
+     "opening price":9290.63
+  },
+  {  
+     "date":"4/28/2018",
+     "closing price":9348.48,
+     "opening price":8939.27
+  }
+]
+
+export default BTCData;
