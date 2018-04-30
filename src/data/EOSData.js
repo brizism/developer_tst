0521b167eecdcad11a308442522b9c51a355120b
brizism <montielbriseida@gmail.com>
2018-04-29 20:32:07 -0400

    add EOS week data

---
+++ src/data/EOSData.js
@@ -0,0 +1,149 @@
+const EOSData = [
+  {
+     "date":"3/31/2018",
+     "closing price":5.99,
+     "opening price":6.11
+  },
+  {
+     "date":"4/1/2018",
+     "closing price":5.66,
+     "opening price":6
+  },
+  {
+     "date":"4/2/2018",
+     "closing price":5.89,
+     "opening price":5.68
+  },
+  {
+     "date":"4/3/2018",
+     "closing price":6.13,
+     "opening price":5.9
+  },
+  {
+     "date":"4/4/2018",
+     "closing price":5.73,
+     "opening price":6.11
+  },
+  {
+     "date":"4/5/2018",
+     "closing price":6.4,
+     "opening price":5.72
+  },
+  {
+     "date":"4/6/2018",
+     "closing price":5.89,
+     "opening price":6.36
+  },
+  {
+     "date":"4/7/2018",
+     "closing price":5.91,
+     "opening price":5.88
+  },
+  {
+     "date":"4/8/2018",
+     "closing price":5.97,
+     "opening price":5.9
+  },
+  {
+     "date":"4/9/2018",
+     "closing price":5.87,
+     "opening price":5.98
+  },
+  {
+     "date":"4/10/2018",
+     "closing price":6,
+     "opening price":5.88
+  },
+  {
+     "date":"4/11/2018",
+     "closing price":8.49,
+     "opening price":6
+  },
+  {
+     "date":"4/12/2018",
+     "closing price":8.7,
+     "opening price":8.5
+  },
+  {
+     "date":"4/13/2018",
+     "closing price":8.74,
+     "opening price":8.71
+  },
+  {
+     "date":"4/14/2018",
+     "closing price":8.6,
+     "opening price":8.75
+  },
+  {
+     "date":"4/15/2018",
+     "closing price":8.4,
+     "opening price":8.63
+  },
+  {
+     "date":"4/16/2018",
+     "closing price":8.1,
+     "opening price":8.39
+  },
+  {
+     "date":"4/17/2018",
+     "closing price":8.51,
+     "opening price":8.13
+  },
+  {
+     "date":"4/18/2018",
+     "closing price":9.03,
+     "opening price":8.49
+  },
+  {
+     "date":"4/19/2018",
+     "closing price":9.44,
+     "opening price":9.02
+  },
+  {
+     "date":"4/20/2018",
+     "closing price":11.45,
+     "opening price":9.51
+  },
+  {
+     "date":"4/21/2018",
+     "closing price":11.11,
+     "opening price":11.58
+  },
+  {
+     "date":"4/22/2018",
+     "closing price":11.35,
+     "opening price":11.07
+  },
+  {
+     "date":"4/23/2018",
+     "closing price":11.61,
+     "opening price":11.33
+  },
+  {
+     "date":"4/24/2018",
+     "closing price":15.31,
+     "opening price":11.73
+  },
+  {
+     "date":"4/25/2018",
+     "closing price":14.46,
+     "opening price":15.1
+  },
+  {
+     "date":"4/26/2018",
+     "closing price":15.49,
+     "opening price":14.43
+  },
+  {
+     "date":"4/27/2018",
+     "closing price":16.67,
+     "opening price":15.47
+  },
+  {
+     "date":"4/28/2018",
+     "closing price":18.87,
+     "opening price":16.69
+  }
+]
+
+export default EOSData;
