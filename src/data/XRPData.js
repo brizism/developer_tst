b0fb7e9566e32c59008a972479d59b5ce7907ec9
brizism <montielbriseida@gmail.com>
2018-04-29 20:09:14 -0400

    add ripple week data

---
+++ src/data/XRPData.js
@@ -0,0 +1,149 @@
+const XRPData = [
+  {
+     "date":"3/31/2018",
+     "closing price":0.512353,
+     "opening price":0.510617
+  },
+  {
+     "date":"4/1/2018",
+     "closing price":0.485081,
+     "opening price":0.513854
+  },
+  {
+     "date":"4/2/2018",
+     "closing price":0.500688,
+     "opening price":0.486067
+  },
+  {
+     "date":"4/3/2018",
+     "closing price":0.558044,
+     "opening price":0.501533
+  },
+  {
+     "date":"4/4/2018",
+     "closing price":0.501631,
+     "opening price":0.554569
+  },
+  {
+     "date":"4/5/2018",
+     "closing price":0.495801,
+     "opening price":0.50195
+  },
+  {
+     "date":"4/6/2018",
+     "closing price":0.476431,
+     "opening price":0.494781
+  },
+  {
+     "date":"4/7/2018",
+     "closing price":0.487867,
+     "opening price":0.476851
+  },
+  {
+     "date":"4/8/2018",
+     "closing price":0.500398,
+     "opening price":0.488227
+  },
+  {
+     "date":"4/9/2018",
+     "closing price":0.491855,
+     "opening price":0.501201
+  },
+  {
+     "date":"4/10/2018",
+     "closing price":0.491931,
+     "opening price":0.492134
+  },
+  {
+     "date":"4/11/2018",
+     "closing price":0.539506,
+     "opening price":0.492568
+  },
+  {
+     "date":"4/12/2018",
+     "closing price":0.628737,
+     "opening price":0.538657
+  },
+  {
+     "date":"4/13/2018",
+     "closing price":0.638087,
+     "opening price":0.630093
+  },
+  {
+     "date":"4/14/2018",
+     "closing price":0.632184,
+     "opening price":0.63738
+  },
+  {
+     "date":"4/15/2018",
+     "closing price":0.682534,
+     "opening price":0.634883
+  },
+  {
+     "date":"4/16/2018",
+     "closing price":0.663297,
+     "opening price":0.682296
+  },
+  {
+     "date":"4/17/2018",
+     "closing price":0.656933,
+     "opening price":0.66489
+  },
+  {
+     "date":"4/18/2018",
+     "closing price":0.708932,
+     "opening price":0.657052
+  },
+  {
+     "date":"4/19/2018",
+     "closing price":0.791976,
+     "opening price":0.712059
+  },
+  {
+     "date":"4/20/2018",
+     "closing price":0.924465,
+     "opening price":0.791172
+  },
+  {
+     "date":"4/21/2018",
+     "closing price":0.873103,
+     "opening price":0.925021
+  },
+  {
+     "date":"4/22/2018",
+     "closing price":0.869995,
+     "opening price":0.86682
+  },
+  {
+     "date":"4/23/2018",
+     "closing price":0.876657,
+     "opening price":0.866578
+  },
+  {
+     "date":"4/24/2018",
+     "closing price":0.934781,
+     "opening price":0.876012
+  },
+  {
+     "date":"4/25/2018",
+     "closing price":0.800607,
+     "opening price":0.932771
+  },
+  {
+     "date":"4/26/2018",
+     "closing price":0.852414,
+     "opening price":0.801774
+  },
+  {
+     "date":"4/27/2018",
+     "closing price":0.813885,
+     "opening price":0.851139
+  },
+  {
+     "date":"4/28/2018",
+     "closing price":0.863804,
+     "opening price":0.812063
+  }
+]
+
+export default XRPData;
