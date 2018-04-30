3ba6a19d60b245a95648af0c66b032248f206747
brizism <montielbriseida@gmail.com>
2018-04-29 20:05:56 -0400

    add ethereum week data

---
+++ src/data/ETHData.js
@@ -0,0 +1,154 @@
+const ETHData = [
+  {
+     "date":"3/30/2018",
+     "closing price":394.65,
+     "opening price":385.91
+  },
+  {
+     "date":"3/31/2018",
+     "closing price":396.46,
+     "opening price":395
+  },
+  {
+     "date":"4/1/2018",
+     "closing price":379.61,
+     "opening price":397.25
+  },
+  {
+     "date":"4/2/2018",
+     "closing price":386.43,
+     "opening price":379.7
+  },
+  {
+     "date":"4/3/2018",
+     "closing price":416.89,
+     "opening price":387.31
+  },
+  {
+     "date":"4/4/2018",
+     "closing price":380.54,
+     "opening price":416.49
+  },
+  {
+     "date":"4/5/2018",
+     "closing price":383.23,
+     "opening price":379.95
+  },
+  {
+     "date":"4/6/2018",
+     "closing price":370.29,
+     "opening price":382.73
+  },
+  {
+     "date":"4/7/2018",
+     "closing price":385.31,
+     "opening price":370.38
+  },
+  {
+     "date":"4/8/2018",
+     "closing price":400.51,
+     "opening price":385.74
+  },
+  {
+     "date":"4/9/2018",
+     "closing price":398.53,
+     "opening price":400.86
+  },
+  {
+     "date":"4/10/2018",
+     "closing price":414.24,
+     "opening price":399.41
+  },
+  {
+     "date":"4/11/2018",
+     "closing price":430.54,
+     "opening price":415.02
+  },
+  {
+     "date":"4/12/2018",
+     "closing price":492.94,
+     "opening price":430.16
+  },
+  {
+     "date":"4/13/2018",
+     "closing price":492.74,
+     "opening price":493.16
+  },
+  {
+     "date":"4/14/2018",
+     "closing price":501.48,
+     "opening price":492.58
+  },
+  {
+     "date":"4/15/2018",
+     "closing price":531.7,
+     "opening price":502.88
+  },
+  {
+     "date":"4/16/2018",
+     "closing price":511.15,
+     "opening price":532.07
+  },
+  {
+     "date":"4/17/2018",
+     "closing price":502.89,
+     "opening price":511.15
+  },
+  {
+     "date":"4/18/2018",
+     "closing price":524.79,
+     "opening price":503.31
+  },
+  {
+     "date":"4/19/2018",
+     "closing price":567.89,
+     "opening price":524.04
+  },
+  {
+     "date":"4/20/2018",
+     "closing price":615.72,
+     "opening price":567.99
+  },
+  {
+     "date":"4/21/2018",
+     "closing price":605.4,
+     "opening price":616
+  },
+  {
+     "date":"4/22/2018",
+     "closing price":621.86,
+     "opening price":606.12
+  },
+  {
+     "date":"4/23/2018",
+     "closing price":642.55,
+     "opening price":621.2
+  },
+  {
+     "date":"4/24/2018",
+     "closing price":708.16,
+     "opening price":643.4
+  },
+  {
+     "date":"4/25/2018",
+     "closing price":615.42,
+     "opening price":707.06
+  },
+  {
+     "date":"4/26/2018",
+     "closing price":662.81,
+     "opening price":618.08
+  },
+  {
+     "date":"4/27/2018",
+     "closing price":647.03,
+     "opening price":662.11
+  },
+  {
+     "date":"4/28/2018",
+     "closing price":683.68,
+     "opening price":644.65
+  }
+]
+
+export default ETHData;
