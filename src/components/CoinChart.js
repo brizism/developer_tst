dccf735039966fd706047d89e8144c6ba347dc8d
brizism <montielbriseida@gmail.com>
2018-04-30 01:23:29 -0400

    update colors of chart

--- src/components/CoinChart.js
+++ src/components/CoinChart.js
@@ -9,18 +9,20 @@ class CoinChart extends Component {
       return null;
     }
     return (
-      <ResponsiveContainer width='75%' height={300}>
-        <AreaChart width={600} height={200} data={data} syncId="anyId" margin={{top: 10, right: 30, left: 0, bottom: 0}}>
-          <CartesianGrid strokeDasharray="3 3"/>
-          <XAxis dataKey="date"/>
-          <YAxis />
-          <Tooltip/>
-          <Legend verticalAlign="top" height={36}/>
-          <Brush />
-          <Area type='monotone' dataKey='closing price' stroke='#82ca9d' fill='#82ca9d' />
-          <Area type='monotone' dataKey='opening price' stroke='#82ca9d' fill='#34ve32' />
-        </AreaChart>
-      </ResponsiveContainer>
+      <div className="coin-chart">
+        <ResponsiveContainer width='100%' height={300}>
+          <AreaChart width={600} height={200} data={data} syncId="anyId" margin={{top: 10, right: 30, left: 0, bottom: 0}}>
+            <CartesianGrid strokeDasharray="3 3"/>
+            <XAxis dataKey="date"/>
+            <YAxis />
+            <Tooltip/>
+            <Legend verticalAlign="top" height={36} />
+            <Brush />
+            <Area type='monotone' dataKey='closing price' stroke='#B810E5' fill='#B810E5' strokeWidth="3px"/>
+            <Area type='monotone' dataKey='opening price' stroke='#34495E' fill='#34495E' strokeWidth="3px"/>
+          </AreaChart>
+        </ResponsiveContainer>
+      </div>
     );
   }
 }
