6bc75189a801fa222133baf378ecf54a0f8cbec7
brizism <montielbriseida@gmail.com>
2018-04-29 20:19:17 -0400

    add bitcoincash week data

---
+++ src/data/BCHData.js
@@ -0,0 +1,149 @@
+const BCHData = [
+  {
+     "date":"3/31/2018",
+     "closing price":685.25,
+     "opening price":697.62
+  },
+  {
+     "date":"4/1/2018",
+     "closing price":643.03,
+     "opening price":688.01
+  },
+  {
+     "date":"4/2/2018",
+     "closing price":662.77,
+     "opening price":644.46
+  },
+  {
+     "date":"4/3/2018",
+     "closing price":712.88,
+     "opening price":664.15
+  },
+  {
+     "date":"4/4/2018",
+     "closing price":649.2,
+     "opening price":710.47
+  },
+  {
+     "date":"4/5/2018",
+     "closing price":646.04,
+     "opening price":648.63
+  },
+  {
+     "date":"4/6/2018",
+     "closing price":610.43,
+     "opening price":641.86
+  },
+  {
+     "date":"4/7/2018",
+     "closing price":640.35,
+     "opening price":611.2
+  },
+  {
+     "date":"4/8/2018",
+     "closing price":655.26,
+     "opening price":640.89
+  },
+  {
+     "date":"4/9/2018",
+     "closing price":637.03,
+     "opening price":656.09
+  },
+  {
+     "date":"4/10/2018",
+     "closing price":649.62,
+     "opening price":637.7
+  },
+  {
+     "date":"4/11/2018",
+     "closing price":666.58,
+     "opening price":648.9
+  },
+  {
+     "date":"4/12/2018",
+     "closing price":734.48,
+     "opening price":666.39
+  },
+  {
+     "date":"4/13/2018",
+     "closing price":738.3,
+     "opening price":735.78
+  },
+  {
+     "date":"4/14/2018",
+     "closing price":738.99,
+     "opening price":738.72
+  },
+  {
+     "date":"4/15/2018",
+     "closing price":780.83,
+     "opening price":741.91
+  },
+  {
+     "date":"4/16/2018",
+     "closing price":766.47,
+     "opening price":781.12
+  },
+  {
+     "date":"4/17/2018",
+     "closing price":757.59,
+     "opening price":766.7
+  },
+  {
+     "date":"4/18/2018",
+     "closing price":888.91,
+     "opening price":758.25
+  },
+  {
+     "date":"4/19/2018",
+     "closing price":968.64,
+     "opening price":890.66
+  },
+  {
+     "date":"4/20/2018",
+     "closing price":1128.02,
+     "opening price":970.15
+  },
+  {
+     "date":"4/21/2018",
+     "closing price":1154.69,
+     "opening price":1126.86
+  },
+  {
+     "date":"4/22/2018",
+     "closing price":1201.22,
+     "opening price":1147.48
+  },
+  {
+     "date":"4/23/2018",
+     "closing price":1432.26,
+     "opening price":1194.71
+  },
+  {
+     "date":"4/24/2018",
+     "closing price":1426.42,
+     "opening price":1434.01
+  },
+  {
+     "date":"4/25/2018",
+     "closing price":1284.79,
+     "opening price":1426.71
+  },
+  {
+     "date":"4/26/2018",
+     "closing price":1420.41,
+     "opening price":1289.54
+  },
+  {
+     "date":"4/27/2018",
+     "closing price":1337.85,
+     "opening price":1422.06
+  },
+  {
+     "date":"4/28/2018",
+     "closing price":1396.25,
+     "opening price":1337.96
+  }
+]
+
+export default BCHData;
